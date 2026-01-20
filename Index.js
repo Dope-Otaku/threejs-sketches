89506cdf18f815d11f4221a72405a010f65a227c
Souvik Halder <fakeyt909@gmail.com>
2026-01-21 00:27:16 +0530

    added the renderer cnavas element in dom directly

--- Index.js
+++ Index.js
@@ -4,3 +4,6 @@ import * as THREE from 'three';
 const h = window.innerHeight;
 const w = window.innerWidth;
 const renderer = new THREE.WebGLRenderer({antialias:true})
+
+renderer.setSize(w,h)
+document.body.appendChild(renderer.domElement) //here we are adding a canvas element directly in document without adding in html
